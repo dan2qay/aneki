9beb79a641841c03073e4e38f2c78d8dd0565d06
dan2qay <dan787978797879@gmail.com>
2022-01-16 19:18:38 +0300

    changes on test file

--- test_word.py
+++ test_word.py
@@ -1,5 +1,7 @@
 from docx import Document
 import httplib2
+import os
+
 
 url = "https://sun9-73.userapi.com/impg/8sVRvYR9nYh3BD54ROTjMRdmpPP8-7pWN6W0xw/iz4CAp2A1x4.jpg?size=56x75&quality=96&sign=e5f67fa97a4abeb7bbc27a52182de412&c_uniq_tag=7J2zlMSRlqoBppvamiFGQh2ePovXJEl9sARBGzgELh4&type=album"
 
@@ -9,7 +11,7 @@ out = open('data/img.jpg', 'wb')
 out.write(content)
 out.close()
 
-document = Document("t.docx")
+document = Document(os.path.abspath('data/t.docx'))
 
-document.add_picture('img.jpg')
-document.save("t.docx")
+document.add_picture('data/img.jpg')
+document.save(os.path.abspath('data/t.docx'))
